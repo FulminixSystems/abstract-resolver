bdef4be81dcf751a8c03688eb9053f7c58e04277
Fulminix Systems <mpratt@fulminixsystems.com>
2026-09-18 04:10:38 -0500

    Refactor tracker.rs for structural analysis

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,3 +1,10 @@
+// ==================================================================================
+// ABSTRACT RESOLVER
+// Copyright (c) 2026 Marcus Pratt / Fulminix Systems. All Rights Reserved.
+// Licensed under the GNU Affero General Public License v3 (AGPLv3).
+// For commercial inquiries, contact the author directly.
+// ==================================================================================
+// tracker.rs
 use crate::lexer::{Token, TokenType};
 use std::ops::Range;
 
