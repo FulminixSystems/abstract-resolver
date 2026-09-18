1d2ce00a84e988b5f0863ebfd013d56d5d666438
Fulminix Systems <mpratt@fulminixsystems.com>
2026-09-18 04:06:14 -0500

    Refactor formatting violation auditing logic

--- src/format.rs
+++ src/format.rs
@@ -1,3 +1,10 @@
+// ==================================================================================
+// ABSTRACT RESOLVER
+// Copyright (c) 2026 Mark Pratt / Fulminix Systems. All Rights Reserved.
+// Licensed under the GNU Affero General Public License v3 (AGPLv3).
+// For commercial inquiries, contact the author directly.
+// ==================================================================================
+// format.rs
 use crate::lexer::Token;
 use crate::tracker::StructuralBlock;
 
